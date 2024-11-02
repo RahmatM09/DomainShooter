0ceb7807983bd3cae007e612ff15fecf46757c10
Rahmat <rahmuh09@gmail.com>
2024-11-03 03:12:32 +0530

    Test Static Mesh

--- Source/DomainShooter/Test/TestActor.cpp
+++ Source/DomainShooter/Test/TestActor.cpp
@@ -9,6 +9,8 @@ ATestActor::ATestActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	TestActorStaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	RootComponent = TestActorStaticMeshComponent;
 }
 
 // Called when the game starts or when spawned
