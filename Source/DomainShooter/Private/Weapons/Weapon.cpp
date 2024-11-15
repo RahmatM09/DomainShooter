bd197fa530fb4ff7d3b01db613b130e99166c098
Rahmat <rahmuh09@gmail.com>
2024-11-16 03:09:06 +0530

    Weapon & Character Overlap

--- Source/DomainShooter/Private/Weapons/Weapon.cpp
+++ Source/DomainShooter/Private/Weapons/Weapon.cpp
@@ -24,6 +24,8 @@ void AWeapon::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	CollisionSphere->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnComponentOverlap);
+	CollisionSphere->OnComponentEndOverlap.AddDynamic(this, &AWeapon::OnComponentEndOverlap);
 }
 
 void AWeapon::Tick(float DeltaTime)
@@ -32,3 +34,12 @@ void AWeapon::Tick(float DeltaTime)
 
 }
 
+void AWeapon::OnComponentOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Error, TEXT("Something Entered!"));
+}
+
+void AWeapon::OnComponentEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Error, TEXT("Something Exited!"));
+}
